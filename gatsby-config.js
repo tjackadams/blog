ca427da704c90c2488fb403070e7af0da16bd405
Thomas Adams <tjackadams@itadams.co.uk>
2020-08-05 11:02:10 +0100

    remove offline support. add favico

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,28 +93,9 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/logos/logo-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        icon: "src/images/favicon.png",
       },
     },
-    "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {

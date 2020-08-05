6724606599969f9c7d96c4e108631729e1e6bbc9
Thomas Adams <tjackadams@itadams.co.uk>
2020-08-05 15:22:18 +0100

    minor styling updates to social share links

---
+++ src/components/SocialIconBar/SocialIconBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Location } from "@reach/router";
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+
+const SocialIconBar = ({ title }) => {
+  return (
+    <Location>
+      {({ location }) => (
+        <>
+          <a
+            href={`https://www.facebook.com/sharer/sharer.php?u=${location.href}`}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="facebook"
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            href={`https://twitter.com/intent/tweet?url=${location.href}&text=${title}`}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="twitter"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href={`https://www.linkedin.com/shareArticle?mini=true&url=${location.href}`}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="linkedin"
+          >
+            <FaLinkedinIn />
+          </a>
+        </>
+      )}
+    </Location>
+  );
+};
+
+export default SocialIconBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer/Footer";
 import SocialInfo from "./SocialInfo/SocialInfo";
 import SEO from "./SEO/SEO";
+import SocialIconBar from "./SocialIconBar/SocialIconBar";
 
-export { Footer, SEO, SocialInfo };
+export { Footer, SEO, SocialIconBar, SocialInfo };

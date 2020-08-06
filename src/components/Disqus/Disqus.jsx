eb5bc8210ca42291552f37e036d940508a5552fc
Thomas Adams <tjackadams@itadams.co.uk>
2020-08-06 15:05:06 +0100

    use official disqus package.

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,51 +1,27 @@
-import React, { Component } from "react";
-import ReactDisqusComments from "react-disqus-comments";
+import React from "react";
+import { DiscussionEmbed } from "disqus-react";
 import urljoin from "url-join";
 import config from "../../../data/SiteConfig";
 
-class Disqus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toasts: [],
-    };
-    this.notifyAboutComment = this.notifyAboutComment.bind(this);
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
+const Disqus = (props) => {
+  const { postNode } = props;
+  if (!config.disqusShortname) {
+    return null;
   }
-
-  onSnackbarDismiss() {
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
-  }
-
-  notifyAboutComment() {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text: "New comment available!" });
-    this.setState({ toasts });
-  }
-
-  render() {
-    const { postNode } = this.props;
-    if (!config.disqusShortname) {
-      return null;
-    }
-    const post = postNode.frontmatter;
-    const url = urljoin(
-      config.siteUrl,
-      config.pathPrefix,
-      postNode.fields.slug
-    );
-    return (
-      <ReactDisqusComments
-        shortname={config.disqusShortname}
-        identifier={post.title}
-        title={post.title}
-        url={url}
-        category_id={post.category_id || null}
-        onNewComment={this.notifyAboutComment}
-      />
-    );
-  }
-}
+  const post = postNode.frontmatter;
+  const url = urljoin(config.siteUrl, config.pathPrefix, postNode.fields.slug);
+  const categoryID = post.category_id || null;
+  return (
+    <DiscussionEmbed
+      shortname={config.disqusShortname}
+      config={{
+        url: url,
+        identifier: post.title,
+        title: post.title,
+        categoryID: categoryID,
+      }}
+    />
+  );
+};
 
 export default Disqus;

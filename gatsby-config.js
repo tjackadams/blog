7973fdb651858a58b5ee37a136b020ceac77f6fd
Thomas Adams <tjackadams@itadams.co.uk>
2022-04-11 19:13:41 +0100

    start fresh project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,31 @@
-const path = require("path");
-const config = require("./data/SiteConfig");
-
 module.exports = {
   siteMetadata: {
-    siteUrl: config.siteUrl,
-    rssMetadata: {
-      site_url: config.siteUrl,
-      feed_url: config.siteUrl + config.siteRss,
-      title: config.siteTitle,
-      description: config.siteDescription,
-      image_url: `${config.siteUrl}/logos/logo-512.png`,
-      copyright: config.copyright,
-    },
+    title: `blog`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-  ]
+  plugins: ["gatsby-plugin-netlify-cms", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-S14YL6K5R1"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
 };
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Disqus from "./Disqus/Disqus";
-import Footer from "./Footer/Footer";
-import ReadNext from "./ReadNext/ReadNext";
-import SocialInfo from "./SocialInfo/SocialInfo";
-import SEO from "./SEO/SEO";
-import SocialIconBar from "./SocialIconBar/SocialIconBar";
-
-export { Disqus, Footer, ReadNext, SEO, SocialIconBar, SocialInfo };

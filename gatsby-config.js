775b4a0ea840e6846b3d6ccb9a8cea8673f78e00
Thomas Adams <tjackadams@itadams.co.uk>
2022-04-12 19:52:28 +0100

    update language

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: "gatsby-plugin-next-seo",
       options: {
         description: config.siteDescription,
-        language: "en_GB",
+        language: "en-GB",
         titleTemplate: `%s | ${config.siteTitle}`,
         title: "home",
         openGraph: {
